4270b457db2fecc4878cff22f74788db1a68f681
John Jensen <jojen@extendhealth.com>
2019-04-25 11:32:15 -0600

    cleaned up structure altered scripts to run in correct database

--- README.txt
+++ README.txt
@@ -2,4 +2,8 @@ Small DB to show use of TsqlT tests.
 The Setup should be done from build.ps1
 Based on SQL Server does require a local instance running at for build to work.
 Migration scripts will need to be be prefixed with a number. This will be addressed later.
-There is a subfodler with a .ps1 to install a sql server express instance.
+There is a subfolder with a .ps1 to install a sql server express instance.
+Clean up will drop objects, and eventually uninstall sql server express. 
+TO-DO: 
+	Write clean up scripts.
+	Rewrite database and schema scripts to be more robust.
--- SqlMigrations/build.ps1
+++ SqlMigrations/build.ps1
@@ -3,10 +3,11 @@ $Server="localhost"
 $Database="master"
 $scriptpath = $MyInvocation.MyCommand.Path
 $dir = Split-Path $scriptpath
+Invoke-Sqlcmd -InputFile "$dir/CreateDB/1_createDataBase.sql" -ServerInstance $Server -Database $Database -ErrorAction 'Stop' -verbose -QueryTimeout 1800 # 30min
+Write-Host  "$dir/CreateDB/1_createDataBase.sql commited" 
+$DatabaseP="Portfolio"
 foreach ($file in Get-ChildItem $dir -Filter "*.sql" | sort-object  -Property name)
 {
-Invoke-Sqlcmd -InputFile $dir/$file -ServerInstance $Server -Database $Database -ErrorAction 'Stop' -verbose -QueryTimeout 1800 # 30min
+Invoke-Sqlcmd -InputFile $dir/$file -ServerInstance $Server -Database $DatabaseP -ErrorAction 'Stop' -verbose -QueryTimeout 1800 # 30min
 Write-Host  "$dir/$file commited" 
 }
-
-   
